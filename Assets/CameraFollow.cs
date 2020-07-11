5001176b75bf4a3e37d3b09e2cd841d6d3be28c8
Somon <alexandre.somon@isen.yncrea.fr>
2020-07-11 18:57:25 +0200

    Need to fix the portal's camera
    see "test" project on desktop, or use it do everything again

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -4,23 +4,28 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    [SerializeField] private Transform MainCamera;
-    [SerializeField] private bool follow = true;
+    [SerializeField] private Transform cameraA;
+    [SerializeField] private Transform portalA;
+    [SerializeField] private Transform portalB;
     private float offset = 0.0f;
     
     // Start is called before the first frame update
     void Awake()
     {
-        offset = Mathf.Abs(MainCamera.position.z - transform.position.z);
+        transform.rotation = cameraA.transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(follow){
-            transform.position = new Vector3(MainCamera.position.x, MainCamera.position.y, MainCamera.position.z-offset);
-            transform.rotation = MainCamera.transform.rotation;
-        }
-        
+        Vector3 playerOffsetFromPortal = cameraA.position - portalB.position;
+        transform.position = portalA.position + playerOffsetFromPortal;
+        print(playerOffsetFromPortal);
+
+        float angularDifferenceBetweenPortalRotations = Quaternion.Angle(portalA.rotation, portalB.rotation);
+
+        Quaternion portalRotationalDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
+        Vector3 newCameraDirection = portalRotationalDifference * cameraA.forward;
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
     }
 }
---
+++ Assets/PortalController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Transform otherPortalTransform;
+    [SerializeField] private Transform mainCamera;
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other) {
+        mainCamera.position = otherPortalTransform.position;
+        print("Transform teleportation");
+    }
+}
